db6ac39ea167b0c2cf11cdf18032fcbad1190774
Anwar076 <121021733+Anwar076@users.noreply.github.com>
2023-03-13 11:52:35 +0100

    varibalen gevuld in meldingenController.php

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -2,16 +2,61 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
-$capaciteit = $_POST['capaciteit']; 
-$melder = $_POST['melder'];
+if(empty($attractie))
+{
+    $errors[] = "Vul de attractie-naam in.";
+}
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+$type = $_POST['type'];
+if(empty($type))
+{
+    $errors[] = "Kies wat voor type.";
+}
+
+$capaciteit = $_POST['capaciteit'];
+if(empty($capaciteit))
+{
+    $errors[] = "Vul voor capaciteit een geldig getal in.";
+}
+
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = true;
+}
+else
+{
+    $prioriteit = false;
+}
+$melder = $_POST['melder'];
+if(empty($melder))
+{
+    $errors[] = "Vul je naam AUB";
+}
+$overig = $_POST['overig'];
+
+
+
+// echo $attractie . " / " . $capaciteit . " / " . $melder;
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
-
+$query = "INSERT INTO meldingen (attractie,  type,  capaciteit, prioriteit, melder, overige_info)
+VALUES (:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 //3. Prepare
-
+$stmt = $conn->prepare($query);
 //4. Execute
+$stmt->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+    ":capaciteit" => $capaciteit,
+    ":prioriteit" => $prioriteit,
+    ":melder" => $melder,
+    ":overige_info" => $overig,
+]);
+
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header("Location:../meldingen/index.php?msg=melding opgeslagen");
